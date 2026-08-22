9dc2be552eeb871f8be8e54106a4ffed4335c68e
Salome Carval <salomecarval6@gmail.com>
2026-08-22 16:27:22 +0530

    backend

---
+++ index.js
@@ -0,0 +1,22 @@
+require ('dotenv').config()
+
+const express = require('express');
+const app = express();
+// 1. Define the port (use the environment variable, or default to 3000 for local testing)
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/login',(req,res)=>{
+  res.send('If in browser http://localhost:4000/login is runned then o/p is seen');
+})
+
+app.get('/html',(req,res)=>{
+  res.send('<h1>Returning HTML. Everytime start server</h1>');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+});
